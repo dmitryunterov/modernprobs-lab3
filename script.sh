56bcd315158bfd543ae82f2521c1afb033b8b775
dmitry <dima@localhost.localdomain>
2016-01-12 19:52:17 +0300

    vrode pashet

---
+++ script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# creating user;
+useradd webmaster > /dev/null
+echo 'Creating user webmaster if needed..................[done]'
+# setting password;
+echo "webmaster:Pa$$w0rd" | chpasswd
+echo 'Creating password for user webmaster..................[done]'
+echo 'New password for webmaster is Pa$$w0rd'
+# installing semanage;
+yum -y install policycoreutils-python > /dev/null
+echo 'Installing semanage if needed..................[done]'
+# link user webmaster with selinux user staff_u;
+semanage login -a -s staff_u -r s0-s0:c0.c1023 webmaster 
+# assigning possible roles for selinux user staff_u;
+semanage user -m -R 'staff_r webadm_r system_r' staff_u
+echo 'making SELinux magic..................[done]'
+# making changes in sudoers config;
+echo 'webmaster ALL=(ALL) TYPE=webadm_t ROLE=webadm_r ALL' > /etc/sudoers.d/webmaster  
+echo 'Creating sudo rules..................[done]'
